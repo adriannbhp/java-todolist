64820c3d9cc15cd8d23caecd90ce198ba3ed3bcf
adriannbhp <adrianbimo43@gmail.com>
2024-04-23 20:58:40 +0700

    commit : menambahkan for loop showTodoList(), testing testShowTodoList()

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -2,16 +2,28 @@ public class AplikasiTodoList {
 
     public static String[] model = new String[10];
     public static void main(String[] args) {
-
-
-
+        testShowTodoList();
     }
 
     /**
      * Menampilkan todo list
      */
     public static void showTodoList(){
+        // Karena membutuhkan index dari array sehingga harus menggunakan fori bukan foreach
+        for (var i = 0; i < model.length; i++) {
+            var todo = model[i];
+            var no = i + 1; // Kenapa i + 1 karena karena iterasi looping dimulai dari 0, sehingga harus ditambah 1
 
+            if(todo != null) {
+                System.out.println(no + ". " + todo);
+            }
+        }
+    }
+
+    public static void testShowTodoList(){
+        model[0] = "Belajar Java Dasar";
+        model[1] = "Studi Kasus Java : Aplikasi Todo List";
+        showTodoList();
     }
 
     /**
@@ -28,4 +40,24 @@ public class AplikasiTodoList {
 
     }
 
+    /**
+     *  Menampilkan view todo list
+     */
+    public static void viewShowTodoList() {
+
+    }
+
+    /**
+     * Menampilkan view add todo list
+     */
+    public static void viewAddTodoList() {
+
+    }
+
+    /**
+     * Menampilkan view remove todo list
+     */
+    public static void viewRemoveTodoList() {
+
+    }
 }
